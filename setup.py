4dd95274b6f1c6e69c8d11392ce1cf6acd883acc
Rocky Xu <34105451+TheRockXu@users.noreply.github.com>
2019-11-25 17:33:44 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='aifin',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Rocky Xu',
+      author_email='percyxu@hotmail.com',
+      url='aitroopers.com',
+      packages=['aifin'],
+     )
